60523fb0739c82bb001dbf3675930f0c22db54ad
xiaomeng9597 <49544781+xiaomeng9597@users.noreply.github.com>
2023-05-13 09:13:30 +0800

    Update ARMv8.sh

--- aarch64/ARMv8.sh
+++ aarch64/ARMv8.sh
@@ -2,3 +2,9 @@
 #sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
 #sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=4.8/g' tools/sed/Makefile
 #sed -i 's/PKG_HASH:=.*/PKG_HASH:=f79b0cfea71b37a8eeec8490db6c5f7ae7719c35587f21edb0617f370eeff633/g' tools/sed/Makefile
+
+
+rm -rf package/luci-theme-argon
+rm -rf package/lean/luci-theme-argon
+rm -rf themes/luci-theme-argon
+rm -rf feeds/luci/themes/luci-theme-argon
